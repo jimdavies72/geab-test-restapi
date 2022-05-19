4f28c5a4867b0f95561a71b9f80eae8a76c84e4a
James D <jimdavies72@googlemail.com>
2022-05-19 14:58:59 +0100

    tidy and attempt at sort fix

--- src/products/productsRoutes.js
+++ src/products/productsRoutes.js
@@ -7,7 +7,7 @@ const {
 const productsRouter = Router();
 
 productsRouter.post("/products", addProduct);
-// using put as get doesnt allow use of req.body
+// using put as 'get' doesnt allow use of req.body
 productsRouter.put("/products/price", listProductsPrice);
 productsRouter.put("/products/group", groupByType);
 
